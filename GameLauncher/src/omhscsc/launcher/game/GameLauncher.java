7370e27423f7a713cce6f41f8feafde6eb8af545
xDest | Zach <destroyer7712@gmail.com>
2016-12-08 23:15:36 -0500

    Added a comment

--- GameLauncher/src/omhscsc/launcher/game/GameLauncher.java
+++ GameLauncher/src/omhscsc/launcher/game/GameLauncher.java
@@ -1,5 +1,13 @@
 package omhscsc.launcher.game;
 
+/**
+ * This class is going to be a mostly unchanged class when finished. The point of this is to launch the game. Whenever we update the game, the launcher
+ * will check to see available updates, and update when necessary. The update will work by downloading the .class files from some server (Probably firebase storage)
+ * and replace the updated files. This shouldn't be extremely difficult I don't think. There is no rush to complete this.
+ * 
+ * @author OMHSCSC
+ *
+ */
 public class GameLauncher {
 
 }
